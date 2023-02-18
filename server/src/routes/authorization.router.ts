ba885469d82a71b608013d68ede5337e31021049
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-02-18 22:08:06 +0200

    `validators` authorization router validation

--- server/src/routes/authorization.router.ts
+++ server/src/routes/authorization.router.ts
@@ -1,5 +1,23 @@
 import {Router} from 'express';
 import {Controller} from '../controllers/authorization.controller';
+import {body} from 'express-validator';
+
 export const router = Router();
 
-router.post('/registration', Controller.Registration);
+router.post(
+  '/registration',
+  body('email').isEmail(),
+  body('username').isString(),
+  body('password').isLength({min: 6, max: 32}),
+  body('userAgent').isString(),
+  Controller.Registration
+);
+router.post(
+  '/login',
+  body('identifier').isString(),
+  body('password').isString(),
+  body('userAgent').isString(),
+  Controller.Login
+);
+
+router.get('/activate/:activationLink', Controller.Activation);
---
+++ server/src/utils/Util.ts
@@ -0,0 +1,35 @@
+import type {Request, Response, APIErrorJSON} from './types/AuthorizationTypes';
+import ApiError from './ApiError';
+import {validationResult} from 'express-validator';
+import {StatusCode} from './types/StatusCode';
+import {UserAgent} from './types/types';
+
+export function sendError(err: unknown, res: Response<APIErrorJSON>) {
+  if (!(err instanceof ApiError)) {
+    console.error(err);
+    const apiErr = ApiError.unhandledError();
+    return res.status(apiErr.code).json(apiErr.toJSON());
+  }
+  return res.status(err.code).json(err.toJSON());
+}
+
+export function validateRequest(req: Request) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new ApiError(
+      StatusCode.BAD_REQUEST,
+      'Validation error received',
+      errors.array()
+    );
+  }
+}
+
+export function validateUserAgent(userAgent: UserAgent) {
+  const validUserAgents = Object.values(UserAgent);
+  if (!validUserAgents.some(agent => agent === userAgent)) {
+    throw new ApiError(
+      StatusCode.BAD_REQUEST,
+      `Expected user agent to be [${validUserAgents}], got '${userAgent}'!`
+    );
+  }
+}
