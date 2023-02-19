c68aa0a84fd120d6e0f1197a3835d2ab84115d62
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-02-19 21:22:50 +0200

    `env` example default environment
    Example of default environment values

--- server/src/environment.d.ts
+++ server/src/environment.d.ts
@@ -2,8 +2,22 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PORT?: string;
+      THIS_API_URL: string;
+      CLIENT_REDIRECT_URI: string;
+
       MONGO_URL: string;
       BCRYPT_SALT: string;
+
+      JWT_SECRET_ACCESS_KEY: string;
+      JWT_ACCESS_EXPIRES_IN: string;
+
+      JWT_SECRET_REFRESH_KEY: string;
+      JWT_REFRESH_EXPIRES_IN: string;
+
+      SMTP_HOST: string;
+      SMTP_PORT: string;
+      SMTP_USER: string;
+      SMTP_PASSWORD: string;
     }
   }
 }
