43c89b6ceb177ca35dab053f1624a593e1fbf338
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-02-22 23:21:35 +0200

    `ui` form components

--- client/src/components/ui/FormButton.tsx
+++ client/src/components/ui/FormButton.tsx
@@ -2,17 +2,14 @@ import "./FormButton.css";
 
 interface ButtonComponentProps {
 	children: string;
+	className: string;
 	filled?: boolean;
 }
 
-const FormButton = ({ children, filled }: ButtonComponentProps) => {
+const FormButton = ({children, filled, className}: ButtonComponentProps) => {
 	return (
-		<div className="form-component">
-			<button className="form-button" style={
-        !filled ? {
-          background: "none"
-        } : {}
-      }>
+		<div className={className}>
+			<button className={"form-button " + (!filled ? "empty-button" : "filled-button")}>
 				{children}
 			</button>
 		</div>
--- client/src/components/ui/FormInput.tsx
+++ client/src/components/ui/FormInput.tsx
@@ -1,24 +1,50 @@
+import { useState } from "react";
 import "./FormInput.css";
 
 interface InputComponentProps {
 	id: string;
 	type: "password" | "text";
 	children: string;
+	className: string;
 }
 
-export default function FormInput({ id, type, children }: InputComponentProps) {
+export default function FormInput({
+	id,
+	type,
+	children,
+	className,
+}: InputComponentProps) {
+	const [visiblity, setVisiblity] = useState(type);
+
+	const passwordVisibility = (
+		event: React.MouseEvent<HTMLElement> & { target: HTMLElement }
+	) => {
+		const isVisible = visiblity === "text";
+		setVisiblity(isVisible ? "password" : "text");
+		event.target.innerText = isVisible ? "visibility" : "visibility_off";
+	};
+
 	return (
-		<div className="form-component">
+		<div className={className}>
 			<input
 				className="form-input"
-				type={type}
+				type={visiblity}
 				id={id}
-        placeholder=" "
-        autoComplete="off"
+				placeholder=" "
+				autoComplete="off"
 			/>
+			{type === "password" && (
+				<i
+					className="material-icons-round input-icon"
+					onClick={passwordVisibility}
+				>
+					visibility
+				</i>
+			)}
 			<label className="form-label" htmlFor={id}>
 				{children}
 			</label>
+			<p className="form-error">Incorrect value*</p>
 		</div>
 	);
 }
