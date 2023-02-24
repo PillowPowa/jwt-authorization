2a8ad5245bf86aa5efd4f46ea0594f9d97350c2a
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-02-24 21:02:27 +0200

    `router` react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import {createContext} from 'react';
 import Registration from './pages/Registration';
+import Login from './pages/Login';
 import './App.css';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import Store from './store/store';
 import type {StoreContext} from './types/types';
@@ -10,10 +16,25 @@ export const Context = createContext<StoreContext>({
   store
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/registration",
+    element: <Registration></Registration>
+  },
+  {
+    path: "/login",
+    element: <Login></Login>
+  }
+]);
+
 function App() {
   return (
     <Context.Provider value={{store}}>
-      <Registration></Registration>
+      <RouterProvider router={router} />
     </Context.Provider>
   );
 }
